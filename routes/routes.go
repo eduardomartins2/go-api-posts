3c100214787df6aeda964c29190509eaf19420e9
Eduardo Martins <eduardomartinsdev2@gmail.com>
2024-08-27 14:04:38 -0300

    Add cors permissinons to debug

--- routes/routes.go
+++ routes/routes.go
@@ -8,14 +8,28 @@ import (
 func SetupRouter() *gin.Engine {
     r := gin.Default()
 
-    // Rota para criação de um post
+    r.Use(corsMiddleware())
+
     r.POST("/posts", controllers.CreatePost)
 
-    // Rota para listar todos os posts
     r.GET("/posts", controllers.GetPosts)
 
-    // Rota para criar um comentário
     r.POST("/comments", controllers.CreateComment)
 
     return r
+}
+
+func corsMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+        
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
+
+        c.Next()
+    }
 }
